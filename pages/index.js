5bf1f2ef430b28097df8cea8de7e8d8c9e8090ab
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-01-29 03:17:57 -0400

    Game tiles added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,58 @@
 import db from "../utils/db";
 import Word from "../models/Word";
+import styles from "../styles/Home.module.css";
 
 export default function Home({ data }) {
-  return <div>{data}</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>VOCÁBULO</h1>
+      <hr className={styles.solid}></hr>
+      <div className={styles.gameWrapper}>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+        <div className={styles.row}>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+          <div className={styles.letterBox}></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export async function getStaticProps() {
