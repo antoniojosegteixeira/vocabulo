15712f271e29afabb4b960141f42d53aca2102b4
Antônio José Teixeira <antoniojosegteixeira@gmail.com>
2022-02-03 20:53:43 -0400

    Removed unused pages

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css"
             integrity="sha512-BnbUDfEUfV0Slx6TunuB042k9tuKe3xrD6q4mg5Ed72LTgzDIcLPxg6yI2gcMFRyomt+yJJxE+zJwNmxki6/RA=="
             crossOrigin="anonymous"
-            referrerpolicy="no-referrer"
+            referrePpolicy="no-referrer"
           />
           <meta
             name="description"
